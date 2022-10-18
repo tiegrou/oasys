7dd676d5ce8573a9fced274752db01cc36aa27f4
tiegrou <105206926+tiegrou@users.noreply.github.com>
2022-10-18 14:27:28 +0200

    start projet

---
+++ oasys/dashboard.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="style.css" >
+    </head>
+    <body>
+
+        <div id=menu style="">
+
+            <h1>Oasys consulting</h1>
+
+            <?php include('menu.php'); ?>
+
+        </div>
+        <div id=conetue>
+
+        </div>
+
+    </body>
+
+</html>
+
+
+<?php
+
+echo("vd")
+
+?>
---
+++ oasys/menu.php
@@ -0,0 +1,26 @@
+
+
+<php>
+    <html>
+
+    <style>
+        .navigation{
+            color: #000000;
+            text-decoration: none;
+        }
+    </style>
+
+    <nav class="navigation">
+        <ul>
+
+            <li><a href="#Dashboard">Dashboard</a></li>
+            <li><a href="#Nouveax projet">Nouveax projet</a></li>
+            <li><a href="#Nouvelle facture">Nouvelle facture</a></li>
+            <li><a href="#Factures">Factures</a></li>
+            <li><a href="#Projets">Projets</a></li>
+            
+        </ul>
+    </nav>
+    </html>
+
+</php>
